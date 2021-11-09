6105bba9eb8ed17d9eb5bf56bf224ea49e9f3998
michaelebelle <61709273+michaelebelle@users.noreply.github.com>
2021-11-09 18:19:58 -0500

    Wordcount.java, collectedResults, screenshots
    Includes screenshots of the execution of the MapReduce job, my tweaked WordCount.java file, and the final results

---
+++ hw4/WordCount.java
@@ -0,0 +1,75 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCount {
+
+  public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+
+    private static final int MISSING = 9999;
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      String line = value.toString();
+      String day = line.substring(15,23);
+      int airTemp;
+      if(line.charAt(87) == '+')
+      {
+        airTemp = Integer.parseInt(line.substring(88,92));
+      } 
+      else
+      {
+        airTemp = Integer.parseInt(line.substring(87,92));
+      }
+
+      String quality = line.substring(92,93);
+
+      if(airTemp != MISSING && quality.matches("[01459]"))
+      {
+        context.write(new Text(day), new IntWritable(airTemp));
+      }
+
+      }
+    }
+  
+
+  public static class IntSumReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int maxValue = Integer.Min_VALUE;
+      for (IntWritable value: values)
+      {
+        maxValue = Math.max(maxValue, max.get());
+      }
+      context.write(key, new IntWritable(maxValue));
+    }
+  }
+  
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(WordCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
