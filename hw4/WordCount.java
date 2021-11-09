db8603116af26f01f07708f3a692917539d22397
michaelebelle <61709273+michaelebelle@users.noreply.github.com>
2021-11-09 18:22:24 -0500

    Update WordCount.java

--- hw4/WordCount.java
+++ hw4/WordCount.java
@@ -49,10 +49,10 @@ public class WordCount {
     public void reduce(Text key, Iterable<IntWritable> values,
                        Context context
                        ) throws IOException, InterruptedException {
-      int maxValue = Integer.Min_VALUE;
+      int maxValue = Integer.MIN_VALUE;
       for (IntWritable value: values)
       {
-        maxValue = Math.max(maxValue, max.get());
+        maxValue = Math.max(maxValue, value.get());
       }
       context.write(key, new IntWritable(maxValue));
     }
